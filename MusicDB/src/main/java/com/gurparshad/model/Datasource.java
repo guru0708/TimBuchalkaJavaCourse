c2c428532e6b3dccc158e9492ae31f084ed54185
Gurparshad Bansal <bansalgurparshad@gmail.com>
2021-04-16 00:58:54 +0530

    change connection string

--- MusicDB/src/main/java/com/gurparshad/model/Datasource.java
+++ MusicDB/src/main/java/com/gurparshad/model/Datasource.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Datasource {
     public static final String DB_NAME = "music.db";
-    public static final String CONNECTION_STRING = "jdbc:sqlite:D:\\data\\training\\oss\\MusicDB\\" + DB_NAME;
+    public static final String CONNECTION_STRING = "jdbc:sqlite:D:\\data\\training\\oss\\TimBuchalkaJavaCourse\\MusicDB\\" + DB_NAME;
 
     public static final String TABLE_ALBUMS = "albums";
     public static final String COLUMN_ALBUM_ID = "_id";
